2120ae039bd777ac48c3b5e3fc79d031a61fda7b
Edi <eduard.lupu@bitstone.eu>
2023-08-21 16:10:36 +0300

    Revert "Started working on the backend of the Shop application."
    This reverts commit f7092c49cb213b7f401c60122268ac5fcfda3c26.

--- src/routes/productRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {getProduct, getProducts} from "../controllers/productController";
-
-const productRouter = express.Router();
-
-productRouter.get('/products', getProducts);
-productRouter.get('/products/:id', getProduct);
-
-export default productRouter;
